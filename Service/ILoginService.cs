4503b2b61767449ec289231794eb1c1b24e5f0ec
RenanBelo <renancbelo12@gmail.com>
2022-06-06 18:51:53 -0300

    Insercao da tela de login

---
+++ Service/ILoginService.cs
@@ -0,0 +1,9 @@
+﻿namespace ForParty.Service
+{
+    public interface ILoginService
+    {
+
+        Task<bool> InserirCadastro(string nome, string cpf, string email, string senha);
+        Task<bool> VerificarLogin(string email, string senha);
+    }
+}
